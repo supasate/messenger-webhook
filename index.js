b55239a026cdd7dcb6ee9239c8e01b0164f9355b
Supasate Choochaisri <supasate.c@gmail.com>
2019-01-06 22:57:23 +0700

    Handle text message

--- index.js
+++ index.js
@@ -3,11 +3,66 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express().use(bodyParser.json());
+const request = require('request');
 
-const port = process.env.PORT || 1337
+const PORT = process.env.PORT || 1337
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+// Handle messages events
+function handleMessage(senderPsid, receivedMessage) {
+  let response;
+
+  // Check if the message contains text
+  console.log('receivedMessage', receivedMessage);
+  if (receivedMessage.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You send the message "${receivedMessage.text}". Now send me an image!`,
+    };
+  }
+
+  // Send the response message
+  console.log('response', response);
+  callSendAPI(senderPsid, response);
+}
+
+
+// Handle messaging_postbacks events
+function handlePostback(senderPsid, receivedPostback) {
+  console.log('receivedPostback', receivedPostback);
+}
+
+// Send response messages via the Send API
+function callSendAPI(senderPsid, response) {
+  // Construct the message body
+  const requestBody = {
+    recipient: {
+      id: senderPsid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: PAGE_ACCESS_TOKEN,
+    },
+    method: 'POST',
+    json: requestBody,
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('Message sent!');
+    } else {
+      console.error('Unable to send message:' + err);
+    }
+  });
+}
+
 
 // Create the endpoint for our webhook
 app.post('/webhook', (req, res) => {
+  console.log('Receive a POST request');
   const body = req.body;
   // Check if this is an event from a page subscription
   if (body.object === 'page') {
@@ -16,7 +71,19 @@ app.post('/webhook', (req, res) => {
       // Get the message. entry.messaging is an array, but
       // will only ever contain one message, so, we get index 0
       const webhookEvent = entry.messaging[0];
-      console.log(webhookEvent)
+      console.log('webhookEvent', webhookEvent);
+
+      // Get the sender PSID
+      const senderPsid = webhookEvent.sender.id;
+      console.log('Sender PSID: ' + senderPsid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhookEvent.message) {
+        handleMessage(senderPsid, webhookEvent.message);
+      } else if (webhookEvent.postback) {
+        handlePostback(senderPsid, webhookEvent.postback);
+      }
     });
 
     // Return a '200 OK' response to all requests
@@ -47,4 +114,4 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Messenger webhook is listening at ${port}`));
+app.listen(PORT, () => console.log(`Messenger webhook is listening at ${PORT}`));
